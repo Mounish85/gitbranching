490a30d8ef849fa91274f5dac5e5d20db8b2cebb
mounish85 <2420030316@klh.edu.in>
2026-08-25 19:19:21 +0530

    Feature:Learn Branching

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 def greet(name):
     print(f"Hello {name},Welcome to GitHub!")
 
+def learn(name):
+    print(f"{name} is learning GitHub Branching,Happy Learning!")
+
 #__main__
 name = input("Enter your name:")
 greet(name)
+learn(name)
 

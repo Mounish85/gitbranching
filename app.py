50b551b71d6e9cc774a90feb0d38d3a2d6a558a2
mounish85 <2420030316@klh.edu.in>
2026-08-25 19:12:43 +0530

    Feature:Greet

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-def greet():
-    print("Hello,Welcome to GitHub!")
+def greet(name):
+    print(f"Hello {name},Welcome to GitHub!")
 
 #__main__
-greet()
+name = input("Enter your name:")
+greet(name)
+
